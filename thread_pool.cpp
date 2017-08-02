e69f5b89c9efbf4d61545c05fc52a63f59f9dc0f
ZihaoWang <razor616811252@gmail.com>
2017-08-02 20:55:22 +0800

    thread_pool

---
+++ thread_pool.cpp
@@ -0,0 +1,32 @@
+#include "./thread_pool.h"
+
+void ThreadPool::thread_entry()
+{
+    while (1)
+    {
+        unique_lock<mutex> l(mut);
+        while (!is_shutdown && job_queue.empty())
+            signal_wakeup.wait(l, [this](){ return is_shutdown || !job_queue.empty(); });
+
+        if (!job_queue.empty())
+        {
+            ++num_busy;
+            auto job = job_queue.front();
+            job_queue.pop();
+            std::promise<double> cost;
+            auto f = cost.get_future();
+            l.unlock();
+
+            job(move(cost));
+
+            l.lock();
+            total_cost += f.get();
+            --num_busy;
+            signal_finished.notify_one();
+        }
+        else if (is_shutdown)
+            break;
+    }
+}
+
+
---
+++ thread_pool.h
@@ -0,0 +1,72 @@
+#ifndef ZIHAO_THREAD_POOL
+#define ZIHAO_THREAD_POOL
+
+#include "./common.h"
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <future>
+#include <condition_variable>
+
+class ThreadPool
+{
+    public:
+    ThreadPool(const size_t num_thread):
+        total_cost(0.0),
+        num_busy(0),
+        is_shutdown(false)
+    {
+        thread_pool.reserve(num_thread);
+        for (size_t i = 0; i < num_thread; ++i)
+            thread_pool.emplace_back(move([this](){ thread_entry(); }));
+    }
+
+    ~ThreadPool()
+    {
+        {
+            // Unblock any threads and tell them to stop
+            unique_lock<mutex> l(mut);
+            is_shutdown = true;
+            signal_wakeup.notify_all();
+        }
+
+        // Wait for all threads to stop
+        cout << "ThreadPool is destroyed and remain threads are joining" << endl;
+        for (auto &thread : thread_pool)
+            thread.join();
+    }
+
+    void do_job(std::function<void(std::promise<double>)> fn)
+    {
+        // Place a job on the queue and unblock a thread
+        unique_lock<mutex> l(mut);
+        job_queue.emplace(move(fn));
+        signal_wakeup.notify_one();
+    }
+
+    double wait_finished()
+    {
+        unique_lock<mutex> l(mut);
+        signal_finished.wait(l, [this](){ return job_queue.empty() && (num_busy == 0); });
+        
+        double tmp = total_cost;
+        total_cost = 0.0;
+        return tmp;
+    }
+
+    protected:
+
+    void thread_entry();
+
+    std::queue<std::function <void(std::promise<double>)>> job_queue;
+    vector<thread> thread_pool;
+    double total_cost;
+
+    mutex mut;
+    condition_variable signal_wakeup;
+    condition_variable signal_finished;
+    size_t num_busy;
+    bool is_shutdown;
+};
+
+#endif
